a979d7e990d94d300d1aaad1e768bedd3c184913
Amir Mohamad <amohamad@my.yorku.ca>
2021-10-29 18:32:50 -0400

    Added test classes for soccergame.model and soccergame.view

---
+++ src/test/java/soccergame/model/ModelTest.java
@@ -0,0 +1,24 @@
+package soccergame.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Collection of tests for the soccergame's model code
+ */
+public class ModelTest {
+
+    /**
+     * A test should have clear documentation on what the test achieves, its preconditions and any
+     * other relevant details.
+     *
+     * ie. This is a sample test template.
+     */
+    @Test
+    void sampleTest() {
+        assertFalse(false);
+        assertTrue(true);
+        assertEquals("", "");
+    }
+}
---
+++ src/test/java/soccergame/view/ViewTest.java
@@ -0,0 +1,23 @@
+package soccergame.view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Collection of tests for the soccergame's view code
+ */
+public class ViewTest {
+    /**
+     * A test should have clear documentation on what the test achieves, its preconditions and any
+     * other relevant details.
+     *
+     * ie. This is a sample test template.
+     */
+    @Test
+    void sampleTest() {
+        assertFalse(false);
+        assertTrue(true);
+        assertEquals("", "");
+    }
+}
