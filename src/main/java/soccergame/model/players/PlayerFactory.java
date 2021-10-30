36b33fe66ed4af33ea1c25b461394702edfd8106
Amir Mohamad <amohamad@my.yorku.ca>
2021-10-29 22:07:42 -0400

    Fixed lowercase bug in factory and moved to players package

--- src/main/java/soccergame/model/players/PlayerFactory.java
+++ src/main/java/soccergame/model/players/PlayerFactory.java
@@ -1,18 +1,14 @@
-package soccergame.model;
-
-import soccergame.model.players.GamePlayer;
-import soccergame.model.players.Goalkeeper;
-import soccergame.model.players.Striker;
+package soccergame.model.players;
 
 import java.awt.*;
 
 public class PlayerFactory {
 
     public GamePlayer getPlayer(String striker) {
-        return switch (striker) {
+        return switch (striker.toLowerCase()) {
             case "striker" -> new Striker("striker", Color.BLUE);
             case "goalkeeper" -> new Goalkeeper("goalkeeper", Color.YELLOW);
-            default -> throw new IllegalArgumentException();
+            default -> throw new IllegalArgumentException("Not a player type.");
         };
     }
 }
