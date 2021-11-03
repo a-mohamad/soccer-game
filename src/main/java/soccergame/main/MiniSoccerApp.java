35821bc966870c73fbe884ec7b7fb61b9bc3ed01
Amir Mohamad <amohamad@my.yorku.ca>
2021-11-03 12:30:41 -0400

    Updated look and feel

--- src/main/java/soccergame/main/MiniSoccerApp.java
+++ src/main/java/soccergame/main/MiniSoccerApp.java
@@ -1,5 +1,6 @@
 package soccergame.main;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import soccergame.controller.GameListener;
 import soccergame.controller.MenubarListener;
 import soccergame.view.GameMenuBar;
@@ -13,6 +14,7 @@ import javax.swing.*;
 public class MiniSoccerApp {
 
     public static void main(String[] args) {
+        enableLookAndFeel();
         JFrame gameFrame = new JFrame("Mini Soccer");
         GamePanel gamePanel = new GamePanel();
         GameListener gameListener = new GameListener(gamePanel);
@@ -26,5 +28,17 @@ public class MiniSoccerApp {
         gameFrame.setLocationRelativeTo(null);
         gameFrame.setResizable(false);
         gameFrame.setVisible(true);
+    }
+
+    /**
+     * Set a custom look and feel.
+     */
+    public static void enableLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (final Exception ex) {
+            System.err.println("Failed to initialize LaF.");
+            ex.printStackTrace();
+        }
     }
 }
